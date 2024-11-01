93903d77c859445d0af03171069e4ae262db8dfd
mahdi1337 <mahdi1337zz@gmail.com>
2024-11-01 03:11:14 +0100

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,12 +1,11 @@
 import pythonmemorymodule
-import base64
 import ctypes
 
 with open('dll.dll', 'rb') as f:
     dll_bin = f.read()
 
-dll = pythonmemorymodule.MemoryModule(data=dll_bin)
-func = dll.get_proc_addr('AddNumbers')
+dll = MemoryModule(data=dll_bin)
+func = dll.get_procedure_address('AddNumbers')
 
 func.argtypes = [ctypes.c_int, ctypes.c_int]  
 func.restype = ctypes.c_int  
